b87d9f1dd2b1ec867f80776f82096683d1797a0b
Koderinjo <mebbon@protonmail.com>
2024-11-05 08:07:53 +0100

    added header

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React, { useContext } from 'react';
+import { DarkModeContext } from '../App';
+
+const Header = () => {
+  const { isDarkMode, setIsDarkMode } = useContext(DarkModeContext);
+
+  return (
+    <header>
+      <div className="container">
+        <a id="sililogo" href="index.html">
+          <img src={isDarkMode ? 'images/sililogo-dark.svg' : 'images/sililogo.svg'} alt="Silicon Logo" />
+        </a>
+        <nav id="main-menu" className="navbar">
+          <a className="nav-link" id="features" href="#">Features</a>
+        </nav>
+        <div id="darkmode-toggle-switch" className="btn-toggle-switch">
+          <span className="label">Dark mode</span>
+          <label htmlFor="darkmode-switch" className="toggle-switch">
+            <input id="darkmode-switch" type="checkbox" checked={isDarkMode} onChange={() => setIsDarkMode(!isDarkMode)} />
+            <span className="slider round"></span>
+          </label>
+        </div>
+        <a id="signin" href="#" className="btn-primary">
+          <i className="fa-light fa-user"></i>
+          <span>Sign in / up</span>
+        </a>
+        <button className="btn-menu" aria-label="Menu Button">
+          <i className="fa-sharp fa-regular fa-bars"></i>
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
