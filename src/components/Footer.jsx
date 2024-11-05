185776bf53858760517b7ad97c9381d540a1cf50
Koderinjo <mebbon@protonmail.com>
2024-11-05 07:53:33 +0100

    added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer>
+    <div className="container">
+      <div className="footer-text">
+        <p className="copyright">© 2024 Silicon. All rights reserved.</p>
+        <p className="credit">Credit MadrasThemes</p>
+      </div>
+    </div>
+  </footer>
+);
+
+export default Footer;
