8f7b00435530ef2db143dc9317e7990fe852775e
Koderinjo <mebbon@protonmail.com>
2024-11-05 06:40:35 +0100

    added brands

---
+++ src/components/Brands.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Brands = () => (
+  <section id="brands">
+    <div className="container">
+      {[1, 2, 3, 4, 5, 6].map(num => (
+        <div key={num} id={`brand-${num}`} className="brand-box">
+          <img src={`src/assets/images/brand-${num}.svg`} alt={`Brand-${num}`} />
+        </div>
+      ))}
+    </div>
+  </section>
+);
+
+export default Brands;
